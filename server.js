04d3d78299ca47e663598cd884f899e24bb11af8
SAIKUMAR P <p.ram4397@gmail.com>
2024-07-19 06:36:17 +0530

    port_name03 update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/user')
 const appointmentRoutes = require('./routes/appointment')
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
@@ -16,8 +16,8 @@ app.use('/api/appointments', appointmentRoutes);
 
 sequelize.sync()
     .then(()=>{
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`)
+        app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`)
         });
     })
     .catch(err => {

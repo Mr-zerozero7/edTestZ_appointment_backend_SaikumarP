e12cc1397c1a443bb49bd8e6ad10ea4b41df8f8a
SAIKUMAR P <p.ram4397@gmail.com>
2024-07-19 12:38:15 +0530

    db authenticate

--- config/db.js
+++ config/db.js
@@ -7,8 +7,15 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
         host: process.env.DB_HOST,
         dialect: 'postgres',
         port: 5432,
-        logging: console.log,
     }
 );
+
+console.log(process.env.DB_NAME)
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  }).catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 module.exports = sequelize;

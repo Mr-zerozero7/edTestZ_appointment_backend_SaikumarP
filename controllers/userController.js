9988dc9b2e617b79c9b66eac7e3104818c4e9969
SAIKUMAR P <p.ram4397@gmail.com>
2024-09-24 00:37:27 +0530

    RequiredFields updated Live backend server

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,9 +11,23 @@ const createUser = async (req, res) =>{
     
     try {
         // check require inputs
-        if(!email || !password){
-            return res.status(400).json({errorMsg: 'Email and Password required'})
+        // if(!username || !email || !password){
+        //     return res.status(400).json({errorMsg: 'Username, Email and Password required'})
+        // }
+
+        const requiredFields = [
+            {field: username, message: 'Username is required'},
+            {field: email, message: 'Email is required'},
+            {field: password, message: 'Password is required'}
+        ];
+
+        for(const {field, message} of requiredFields){
+            if(!field){
+                return res.status(400).json({errorMsg: message});
+            }
         }
+
+
         //Find User Details by Email
         const emailCheck = await User.findOne({where: {email}});
         if(emailCheck){
@@ -58,18 +72,22 @@ const loginUser = async(req, res) => {
             return res.status(404).json({errorMsg: 'User Not Found...'})
         }
 
+        if(password === ''){
+            return res.status(404).json({errorMsg: 'Please Enter Password...'})
+        }
         // Check Password Match
         const comparePass = await bcrypt.compareSync(password, user.password)
         if(!comparePass){
             res.status(404).json({msg: 'Please Enter Correct Password'})
+        }else{
+            const token = generateToken(user)
+            // const token = jwt.sign({id: user.id, email: user.email}, process.env.SECRET_KEY, {expiresIn: '1d'});
+            const {username} = user
+    
+            //handle Successfull Login
+            res.status(200).json({message: 'Login Successfull', username, token})
         }
 
-        const token = generateToken(user)
-        // const token = jwt.sign({id: user.id, email: user.email}, process.env.SECRET_KEY, {expiresIn: '1d'});
-        const {username} = user
-
-        //handle Successfull Login
-        res.status(200).json({message: 'Login Successfull', username, token})
     } catch (error) {
         console.log('Error fetching users:', error.message);
         res.status(500).json({error: 'Internal Server error'});

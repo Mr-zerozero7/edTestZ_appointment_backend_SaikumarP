0c4e2986f749746b684fc9a7e1cf4cf378912390
P SAIKUMAR <p.ram4397@gmail.com>
2024-11-26 00:53:21 +0530

    remote DB changed to aiven

--- config/db.js
+++ config/db.js
@@ -7,6 +7,12 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
         host: process.env.DB_HOST,
         dialect: 'postgres',
         port: process.env.DB_PORT,
+        dialectOptions:{
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        },
         connectionTimeoutMillis: 2000
       }
     );
